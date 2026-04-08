e778ffed7a5cfef4c8fbdfd4108d939a0e0a2eea
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2026-04-08 17:44:46 -0400

    Add goahk helper for window inventory action

--- examples/standalone-script/main.go
+++ examples/standalone-script/main.go
@@ -12,6 +12,8 @@ func main() {
 
 	// Press "1" to show a MessageBox. The process stays running until Escape is pressed.
 	app.Bind("1", goahk.MessageBox("goahk", "You pressed 1"))
+	// Press "2" to snapshot open applications into metadata key "open_apps".
+	app.Bind("2", goahk.ListOpenApplications("open_apps"), goahk.Log("Captured open app inventory to metadata.open_apps"))
 	app.Bind("Escape", goahk.Stop())
 
 	if err := app.Run(context.Background()); err != nil {
