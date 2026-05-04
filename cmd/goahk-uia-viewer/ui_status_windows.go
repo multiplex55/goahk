43fa360608dc8e3dc4394e6e38fc7368b5017c2f
multiplex55 <6619098+multiplex55@users.noreply.github.com>
2026-05-04 18:56:15 -0400

    Refactor UI viewer updates to single UI-thread boundary

--- cmd/goahk-uia-viewer/ui_status_windows.go
+++ cmd/goahk-uia-viewer/ui_status_windows.go
@@ -3,12 +3,10 @@
 package main
 
 func (ui *viewerUI) setStatus(text string) {
-	ui.dispatcher.Queue(func() {
-		if ui.statusText != nil {
-			ui.statusText.SetText(text)
-		}
-		if ui.statusBar != nil {
-			ui.statusBar.Invalidate()
-		}
-	})
+	if ui.statusText != nil {
+		ui.statusText.SetText(text)
+	}
+	if ui.statusBar != nil {
+		ui.statusBar.Invalidate()
+	}
 }
